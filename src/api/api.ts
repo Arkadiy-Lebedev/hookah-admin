906304771e8cc6e04b7586828ca0d97f6936cbdc
Arkady <bepluvdt@gmail.com>
2023-07-18 14:16:54 +0300

    add server

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,2 @@
-export const apiMain = "http://localhost:8000/"
+// export const apiMain = "http://localhost:8000/"
+export const apiMain = "https://myhookah.ru/"
--- src/socket.js
+++ src/socket.js
@@ -1,45 +1,44 @@
-import { reactive } from "vue";
-import { io } from "socket.io-client";
-import { useTablesList } from './stores/tablesStore'
+import { io } from 'socket.io-client'
+import { reactive } from 'vue'
 import { useDateStore } from './stores/dateStore'
-const dateStore = useDateStore();
+import { useTablesList } from './stores/tablesStore'
+const dateStore = useDateStore()
 
-const tablesList = useTablesList();
+const tablesList = useTablesList()
 
 export const state = reactive({
-    connected: false,
-    fooEvents: [],
+  connected: false,
+  fooEvents: [],
   barEvents: []
-});
+})
 
 const upLoad = async () => {
-    await tablesList.getTablesInDate(dateStore.dateInBooking)
-
+  await tablesList.getTablesInDate(dateStore.dateInBooking)
 }
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:8000";
+// const URL = "http://localhost:8000";
+const URL = 'https://myhookah.ru'
 
-export const socket = io(URL);
+export const socket = io(URL)
 
-socket.on("update", () => {
-    upLoad()
-  
-});
+socket.on('update', () => {
+  upLoad()
+})
 
-socket.on("connect", (socket) => {
-    console.log("connect", socket)
-  state.connected = true;
-});
+socket.on('connect', (socket) => {
+  console.log('connect', socket)
+  state.connected = true
+})
 
-socket.on("disconnect", () => {
-  state.connected = false;
-});
+socket.on('disconnect', () => {
+  state.connected = false
+})
 
-socket.on("foo", (...args) => {
-  state.fooEvents.push(args);
-});
+socket.on('foo', (...args) => {
+  state.fooEvents.push(args)
+})
 
-socket.on("bar", (...args) => {
-  state.barEvents.push(args);
-});
+socket.on('bar', (...args) => {
+  state.barEvents.push(args)
+})
